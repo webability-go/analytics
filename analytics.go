81839dbbd65e9807adfabcfdf84d8c4458a4eab6
metalwolf <phil@thomassigny.org>
2020-04-12 13:57:36 -0500

    patch v0.1.0

--- analytics.go
+++ analytics.go
@@ -1,64 +1,64 @@
 package analytics
 
 import (
-  "fmt"
-  "strings"
-  "math/rand"
-  "net/url"
-  "net/http"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"strings"
 )
 
 const VERSION = "0.1.0"
 
 type UA struct {
-  ID string
-  UUID string
-  GeoID string
+	ID    string
+	UUID  string
+	GeoID string
 }
 
 func CreateUA(id string) *UA {
-  return &UA{ID: id,}
+	return &UA{ID: id}
 }
 
-func (ua *UA)SetUUID(uuid string) {
-  ua.UUID = uuid
+func (ua *UA) SetUUID(uuid string) {
+	ua.UUID = uuid
 }
 
-func (ua *UA)SetGeoID(geoid string) {
-  ua.GeoID = geoid
+func (ua *UA) SetGeoID(geoid string) {
+	ua.GeoID = geoid
 }
 
-func (ua *UA)Event(category string, action string, label string, value int) {
+func (ua *UA) Event(category string, action string, label string, value int) {
 
-  hc := http.Client{}
-  
-  form := url.Values{}
-  form.Add("v", "1")
-  form.Add("tid", ua.ID)
-  if ua.UUID == "" {
-    ua.UUID = UUID() // cannot be null
-  }
-  form.Add("cid", ua.UUID)
-  form.Add("t", "event")
-  form.Add("ec", category)
-  form.Add("ea", action)
-  form.Add("el", label)
-  form.Add("ev", fmt.Sprint(value))
-  if ua.GeoID != "" {
-    form.Add("geoid", ua.GeoID)
-  }
+	hc := http.Client{}
 
-  req, _ := http.NewRequest("POST", "https://www.google-analytics.com/collect", strings.NewReader(form.Encode()))
-  req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-  hc.Do(req)
+	form := url.Values{}
+	form.Add("v", "1")
+	form.Add("tid", ua.ID)
+	if ua.UUID == "" {
+		ua.UUID = UUID() // cannot be null
+	}
+	form.Add("cid", ua.UUID)
+	form.Add("t", "event")
+	form.Add("ec", category)
+	form.Add("ea", action)
+	form.Add("el", label)
+	form.Add("ev", fmt.Sprint(value))
+	if ua.GeoID != "" {
+		form.Add("geoid", ua.GeoID)
+	}
+
+	req, _ := http.NewRequest("POST", "https://www.google-analytics.com/collect", strings.NewReader(form.Encode()))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	hc.Do(req)
 }
 
 func UUID() string {
-  b := make([]byte, 16)
-  _, err := rand.Read(b)
-  if err != nil {
-    return ""
-  }
-  uuid := fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
-  return uuid
+	b := make([]byte, 16)
+	_, err := rand.Read(b)
+	if err != nil {
+		return ""
+	}
+	uuid := fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
+	return uuid
 }
--- testunit/analytics_test.go
+++ testunit/analytics_test.go
@@ -1,23 +1,20 @@
 package main
 
 import (
-  "fmt"
-  "testing"
+	"fmt"
+	"testing"
 
-  "github.com/webability-go/analytics"
+	"github.com/webability-go/analytics"
 )
 
 var UA *analytics.UA
 
 func TestAnalytics(t *testing.T) {
-  
-  UA = analytics.CreateUA("UA-11441155-8")
-  UA.SetUUID(analytics.UUID())
-  UA.SetGeoID("FR")
-  UA.Event("alexa-go", "test-unit", "country", 0)
-  
-  fmt.Println("OK")
+
+	UA = analytics.CreateUA("UA-11441155-8")
+	UA.SetUUID(analytics.UUID())
+	UA.SetGeoID("FR")
+	UA.Event("alexa-go", "test-unit", "country", 0)
+
+	fmt.Println("OK")
 }
-
-
-
